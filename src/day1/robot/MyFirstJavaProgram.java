2455637e056906862a6e3442c6c35199e96c5f25
dacoolkid <league@WorthyMini-3.attlocal.net>
2017-03-12 15:12:40 -0700

    Wow

--- src/day1/robot/MyFirstJavaProgram.java
+++ src/day1/robot/MyFirstJavaProgram.java
@@ -4,10 +4,20 @@ import java.awt.Color;
 import org.jointheleague.graphical.robot.Robot;
 
 public class MyFirstJavaProgram {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		
 		// START HERE
+	
+Robot rawrxd = new Robot();
 
+for (int i = 0; i < 360; i++) {
+	rawrxd.penDown();
+	rawrxd.setRandomPenColor();
+	rawrxd.move(5);
+	rawrxd.turn(1);
+	
+	
+}
 	
 		
 		
